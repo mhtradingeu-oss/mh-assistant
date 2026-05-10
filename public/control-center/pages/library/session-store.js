1416b9a604a98acc0d2bf419d3408cdb98f6d932
Nadeem <mhtradingeu@gmail.com>
2026-05-10 21:40:39 +0000

    Add Library session store module

---
+++ public/control-center/pages/library/session-store.js
@@ -0,0 +1,43 @@
+const DEFAULT_LIBRARY_SESSION = Object.freeze({
+  selectedAssetId: "",
+  searchQuery: "",
+  typeFilter: "all",
+  statusFilter: "all",
+  sourceFilter: "all",
+  folderFilter: "all",
+  viewMode: "grid",
+  page: 1,
+  pageSize: 24,
+  expandedPanels: Object.freeze({}),
+  uploadInProgress: false
+});
+
+export function createLibrarySession(overrides = {}) {
+  return {
+    ...DEFAULT_LIBRARY_SESSION,
+    ...overrides,
+    expandedPanels: {
+      ...DEFAULT_LIBRARY_SESSION.expandedPanels,
+      ...(overrides.expandedPanels || {})
+    }
+  };
+}
+
+export function normalizeLibrarySession(session = {}) {
+  return createLibrarySession(session);
+}
+
+export function updateLibrarySession(session = {}, patch = {}) {
+  return normalizeLibrarySession({
+    ...session,
+    ...patch,
+    expandedPanels: {
+      ...(session.expandedPanels || {}),
+      ...(patch.expandedPanels || {})
+    }
+  });
+}
+
+export function isLibraryTransientKey(key) {
+  return Object.prototype.hasOwnProperty.call(DEFAULT_LIBRARY_SESSION, key);
+}
